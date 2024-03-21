97caca3722767024949420d02911f2caec956790
lidiia <lidiiadolfin@gmail.com>
2024-03-21 22:46:20 +0200

    feat:stringgz

---
+++ JSstringdz/js/script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let result = 5 + 5 + '5';
+console.log(result);
+
+let email = 'lidiiadolfin@gmail.com';
+console.log(email.length, email.includes('@'));
+
+let name = 'name';
+let my = 'my';
+let is ='is';
+let fullName = `${my} ${name} ${is} Viktor`
+console.log(fullName);
+
+
+
+let userName = 'Олександро';
+let payment = '300 гривень'
+alert(`Дякуємо, ${userName}! До сплати ${payment}`);
