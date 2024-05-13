7b98e406512eb25a8a1f90f04e707eaf0c6b0dfc
lidiia <lidiiadolfin@gmail.com>
2024-05-13 16:01:17 +0300

    feat:masivi

---
+++ JSmasivimetodu/js/script.js
@@ -0,0 +1,78 @@
+'use strict'
+
+// 1
+// Напиши скрипт, який об'єднує всі елементи массива в один рядок.  Елементів може бути довільна кількість. Нехай елементи массива  в рядку будут розділені комою.
+//  - Спочатку через for
+//  - Потім через join()
+
+const friends =['Mango', 'Poly', 'Kiwi', 'Ajax'];
+let string = '';
+
+for(let i = 0; i < friends.length; i++) {
+    string += `${friends[i]} `
+}
+console.log(string);
+
+
+string = friends.join(', ');
+console.log(string);
+
+
+
+//2
+// Працюємо з колекцією карток в trello.  Метод splice() (можна використати інші методи)
+
+// const cards = [
+//     'Карточка-1',
+//     'Карточка-2',
+//     'Карточка-3',
+//     'Карточка-4',
+//     'Карточка-5',
+//  ];
+
+
+//3
+//   Видалити
+// Видалення (по індексу), метод indexOf()
+
+//  const cardToRemove = 'Карточка-3';
+//  const indexToRemove = cards.indexOf(cardToRemove)
+//  if(indexToRemove !== -1) {
+//     cards.splice(indexToRemove, 1)
+//  }
+//  console.log(cards);
+
+// cards.splice(2, 1)
+// console.log(cards);
+
+
+
+//4
+//Додати
+// Додавання (//!по індексу)
+
+const cards = [
+    'Карточка-1',
+    'Карточка-2',
+    'Карточка-3',
+    'Карточка-4',
+    'Карточка-5',
+ ];
+// cards.push('Карточка-6')
+// console.log(cards);
+//or
+const cardToInsert = 'Карточка-6';
+const cardsToAdd = cards.indexOf(cardToInsert)
+if(cardsToAdd === cards.length+1) {
+    cards.push(cardsToAdd)
+}
+console.log(cards);
+
+//5
+
+const cardToUpdate = 'Карточка-4'
+const indexToUpdate = cards.indexOf(cardToUpdate)
+if(indexToUpdate !== -1) {
+    cards[indexToUpdate] = 'оновленно карт-4'
+}
+console.log(cards);
