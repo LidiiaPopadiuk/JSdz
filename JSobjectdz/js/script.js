8fbd7697b46b61a36b4eb10a52c9b5aaf4b029e0
lidiia <lidiiadolfin@gmail.com>
2024-05-23 23:12:58 +0300

    feat: functionDZ

--- JSobjectdz/js/script.js
+++ JSobjectdz/js/script.js
@@ -3,56 +3,104 @@
 //1
 //Створіть об'єкт "bankAccount" з властивостями "ownerName", "accountNumber", "balance". Додайте до об'єкту метод "deposit", який дозволяє додавати гроші на рахунок, та метод "withdraw", який дозволяє знімати гроші з рахунку. Методи повинні зпрацьовувати при натисканні на кнопки “поповнити рахунок” та отримати ‘готівку’ відповідно. Після проведення операції виводити повідомлення про залишок на рахунку.
 
+let string = prompt(
+  `У вас на рахунку ${this.balance}. Хечете поповнити рахунок?`,
+  ""
+);
+let question = prompt("Введіть суму поповнення коштів");
+let newBalance;
+
 const bankAccount = {
   ownerName: "Ivan",
   accountNumber: 108498274,
   balance: 100000,
 
-  deposit() {},
+  deposit(add) {
+    if (string === true) {
+      newBalance === this.balance + add;
+    } else {
+      alert("Дякую, що звернулися до нас");
+    }
+  },
+  // withdraw(rest) {
+  //   let string = prompt
+  // }
 };
+bankAccount.deposit();
 
-//2
+//* 2
 //Створіть об'єкт "weather" з властивостями "temperature", "humidity", "windSpeed". Додайте до об'єкту метод, який повертає "true", якщо температура нижче 0 градусів Цельсія, та "false", якщо температура вище або рівна 0 градусів Цельсія. Температуру потрібно отримати з інпуту на сторінці. Якщо метод повернув "true" вивести повідомлення “температура нижче 0 градусів Цельсія” і навпаки
 
 const weather = {
-  temperature: "",
+  temperature: -5,
   humidity: "40%",
   windSpeed: "2km/h",
 
-  resultTemperature(temp) {
-    if (temp <= 0) {
-      console.log("температура < 0 градусів Цельсія");
+  resultTemperature() {
+    if (this.temperature < 0) {
+      console.log("Так, температура < 0 градусів Цельсія");
     } else {
-      console.log("температура > 0 градусів Цельсія");
+      console.log("Ні, температура > 0 градусів Цельсія");
     }
   },
-  this.temperature = temp
 };
-weather.resultTemperature(5)
+weather.resultTemperature();
 
-
-//3
+//? 3
 //Створіть об’єкт "user", якbq буде мати властивості "name", "email", "password". Додайте метод "login", який буде перевіряти правильність введеного email та password. Використайте інпути для запису значень властивостей в об’єкт
 
 const user = {
-    name: 'Ivan',
-    email: 'ivan3@gmail.com',
-    password: '123jkT',
+  name: "Ivan",
+  email: "ivan3@gmail.com",
+  password: "123jkT",
 
-    addLogin() {
-        if(this.email.includes('@' && '.')) {
-
-        }
+  addLogin() {
+    if (this.email.includes("@" && ".")) {
+      console.log('Все прийнято');
+    } else {
+      console.log('Перевірте gmail');
     }
-}
 
+    if(this.password > 5) {
+      console.log('Операція пройшла успішно');
+    } else {
+      console.log('щось пішло не так');
+    }
+  }
+};
+user.addLogin()
 
-//4
+//* 4
 //Створіть об'єкт "movie" з властивостями "title", "director", "year", "rating". Додайте до об'єкту метод, який повертає "true", якщо рейтинг фільму вище 8, та "false", якщо рейтинг фільму 8 або нижче. Вивести значення властивостей на сторінку. Якщо метод повернув "true" то змінити колір тексту поля title на зелений.
 
 const movie = {
-    title: 'Madagascar',
-    director: 'Eric darnell',
-    year: 2015,
-    rating: '9/10'
-}
+  title: "Madagascar",
+  director: "Eric darnell",
+  year: 2015,
+  rating: 7,
+
+  checkRaiting() {
+    if (this.rating >= 8) {
+      console.log('true');
+    } else {
+      console.log('false');
+    }
+  }
+};
+movie.checkRaiting()
+
+// const movie = {
+//   title: "Madagascar",
+//   director: "Eric darnell",
+//   year: 2015,
+//   rating: "",
+
+//   checkRaiting(number) {
+//     if (number >= 8) {
+//       console.log("true");
+//     } else {
+//       console.log("false");
+//     }
+//   },
+// };
+// movie.checkRaiting(8);
---
+++ JSstringFunctiondz/js/script.js
@@ -0,0 +1,85 @@
+"use strict";
+
+//? 1
+//Напиши скрипт який при кліку на кнопку буде виводити на екран повідомлення “Hello World!”. Використовуй колбек в вигляді стрілки
+
+// let message = confirm("Хочете повідомлення?");
+// if (message === true) {
+//   alert("Hello world");
+// } else {
+//   alert("Sorry but you don't want message");
+// }
+
+
+//? 2
+// Створи програму, яка генерує випадкове число від 1 до 100. Користувач повинен вгадати число, введенням його в текстове поле. При кліку на кнопку "Перевірити" програма повинна повідомити користувача, чи є їх відповідь правильною.2
+
+const randNumberFn = (callback) => {
+  const max = 100;
+  const min = 1;
+  const randomNumber = Math.random() * (max - min) + min;
+  const randomNumberAnswer = +prompt('Вгадайте число від 1 до 100', '')
+
+  if(randomNumberAnswer > 100) {
+   return alert('загадане число є меньше 100')
+  
+  }
+  if(randomNumber === randomNumberAnswer) {
+    return alert('Молодець, все вірно!');
+  } else {
+    return alert(`Спробуй ще раз! Правильна відповідь: ${Math.round(randomNumber)}`);
+  }
+  callback(randomNumber)
+};
+const showNumber = (result) => {
+    return alert(result);
+}
+randNumberFn()
+
+//! 3
+//Створи програму, яка відображає повідомлення про те, скільки разів користувач клікнув на сторінці.
+
+//? *4
+//Напиши функцію, яка приймає масив чисел і колбек-функцію. Функція повинна застосовувати колбек-функцію до кожного елементу масиву та повертати новий масив, що містить результати застосування колбек-функції до кожного елементу.
+
+let arr = [1, 2, 3, 4, 5]
+const squareArray = (array, callback) => {
+   let newArr = [];
+  //  for(let i = 0; i < array.length; i++) {
+  //   newArr.push(array[i] * array[i])
+  //  }
+  for(let num of array) {
+    newArr.push(callback(num))
+  }
+   return newArr
+}
+const squereCallback = (num) => {
+  return num * num
+}
+
+console.log(squareArray(arr,squereCallback));
+
+
+
+// const applyCallbackToEachElement = (arr, callback) => {
+// }
+// const arr = [1, 2, 3, 4, 5]
+// const squereCallback = ;
+// const result = applyCallbackToEachElement(arr, squereCallback);
+// console.log(result);//1 4 9 16 25
+
+//* 5
+//Розрахунок дисконтної ціни
+//Створіть функцію, яка розраховує дисконтну ціну на товар з урахуванням знижки. Функція повинна приймати вартість товару та відсоток знижки як аргументи. Використайте стрілкову функцію та колбек.
+
+const calculareDiscountedPrice = (price, discount, callback) => {
+  let discountedPrice = (price * (100 - discount)) / 100;
+  callback(discountedPrice);
+};
+
+const showdiscountedPrice = (result) => {
+  console.log(result);
+};
+
+calculareDiscountedPrice(100, 10, showdiscountedPrice); // 90
+calculareDiscountedPrice(250, 20, showdiscountedPrice); //200
